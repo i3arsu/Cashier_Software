ade2955c6b30829a9f18cdaf96305d1bec7066e9
coco <kokotenrico@gmail.com>
2022-02-10 10:16:05 +0100

    Distribute functionalities on screen

--- register/src/App.tsx
+++ register/src/App.tsx
@@ -4,15 +4,15 @@ export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Half-section">
+          <div className="Box">Cart</div>
+          <div className="Box">Buttons</div>
+          <div className="Box">Calculator</div>
+        </div>
+        <div className="Half-section">
+          <div className="Box">Item List</div>
+          <div className="Box">Search Bar</div>
+        </div>
       </header>
     </div>
   );
