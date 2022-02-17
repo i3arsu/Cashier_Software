7c94dfb29eb34896a7cf307c3b1ba8c240697904
coco <kokotenrico@gmail.com>
2022-02-17 10:44:48 +0100

    Implement Mapping Icon to Category

--- register/src/ItemList.tsx
+++ register/src/ItemList.tsx
@@ -2,9 +2,22 @@ import { useItems } from "./store";
 import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import "./App.css";
+import { FaBeer, FaCoffee, FaIceCream, FaGlassWhiskey } from "react-icons/fa";
 
 export const ItemList = observer(() => {
   const itemStore = useItems();
+
+  const categories: { [id: string]: JSX.Element } = {
+    0: <FaBeer />,
+    1: <FaCoffee />,
+    2: <FaIceCream />,
+    3: <FaGlassWhiskey />,
+  };
+
+  const returnCategoryIcon = (id: string): JSX.Element => {
+    const key = parseInt(id) % 4;
+    return categories[key];
+  };
 
   useEffect(() => {
     itemStore.fetchItems();
@@ -20,7 +33,8 @@ export const ItemList = observer(() => {
                 className="Item-box"
                 onClick={() => itemStore.addItemToCart(item.uid)}
               >
-                {item.name}
+                <div>{item.name}</div>
+                <div className="Icon">{returnCategoryIcon(item.uid)}</div>
               </div>
             )
           )
@@ -30,7 +44,8 @@ export const ItemList = observer(() => {
                 className="Item-box"
                 onClick={() => itemStore.addItemToCart(item.uid)}
               >
-                {item.name}
+                <div>{item.name}</div>
+                <div className="Icon">{returnCategoryIcon(item.uid)}</div>
               </div>
             )
           )}
