1427d20242b230783683b4e78c3ab9071e09964b
Teo1121 <teo.ferenac@gmail.com>
2022-02-17 13:01:05 +0100

    Refactored ItemResponse layout

--- src/main/java/net/unipu/CashierSoftwareAPI/payload/response/ItemResponse.java
+++ src/main/java/net/unipu/CashierSoftwareAPI/payload/response/ItemResponse.java
@@ -1,21 +1,60 @@
 package net.unipu.CashierSoftwareAPI.payload.response;
 
+
 import net.unipu.CashierSoftwareAPI.models.Item;
 
-import java.util.List;
-
 public class ItemResponse {
-    private Item item;
+    private String id;
+    private String name;
+    private float price;
+    private String category;
+
+    public ItemResponse(String id, String name, float price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 
     public ItemResponse(Item item) {
-        this.item = item;
+        this.id = item.getId().toString();
+        this.name = item.getName();
+        this.price = item.getPrice();
+        this.category = item.getCategory().getName().name();
     }
 
-    public Item getItem() {
-        return item;
+    public String getId() {
+        return id;
     }
 
-    public void setItems(Item item) {
-        this.item = item;
+    public void setId(String id) {
+        this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+
 }
